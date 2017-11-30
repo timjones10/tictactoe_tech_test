f9eb58eebd67b9ff3ea14b780ea0efa6975a8d79
Tim Jones <timjones10@hotmail.com>
2017-11-30 11:52:23 +0000

    Field object created which has a content property which can be set. This field object will form the basis of a board object

---
+++ lib/field.rb
@@ -0,0 +1,9 @@
+class Field
+
+attr_accessor :content
+
+def inititalize
+@content = content
+end
+
+end
--- spec/field_spec.rb
+++ spec/field_spec.rb
@@ -2,11 +2,11 @@ require 'field'
 
 RSpec.describe 'A field' do
 
-  let(:field) {Field.new('X')}
+  let(:field) {Field.new}
 
   it 'has content which can be set to X or O' do
-    field.content = '0'
-    expect(field.content).not_to eq 'X'
+    field.content= '0'
+    expect(field.content).to eq '0'
   end
 
 end
