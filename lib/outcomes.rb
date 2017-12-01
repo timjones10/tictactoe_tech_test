a6c3ff7ebf23af8d367d1859fd79f338691c2bef
Tim Jones <timjones10@hotmail.com>
2017-12-01 13:59:10 +0000

    Outcome tests increased to 100% coverage but requires refactoring

--- lib/outcomes.rb
+++ lib/outcomes.rb
@@ -1,4 +1,5 @@
 class Outcomes
+
   def check(results)
     return 'win' if results[0] == 'X' && results[1] == 'X' && results[2] == 'X'
     return 'win' if results[0] == '0' && results[1] == '0' && results[2] == '0'
@@ -14,7 +15,19 @@ class Outcomes
     return 'win' if results[2] == '0' && results[5] == '0' && results[8] == '0'
     return 'win' if results[0] == 'X' && results[4] == 'X' && results[8] == 'X'
     return 'win' if results[0] == '0' && results[4] == '0' && results[8] == '0'
-    return 'win' if results[2] == 'X' && results[4] == 'X' && results[7] == 'X'
-    return 'win' if results[2] == '0' && results[4] == '0' && results[7] == '0'
+    return 'win' if results[2] == 'X' && results[4] == 'X' && results[6] == 'X'
+    return 'win' if results[2] == '0' && results[4] == '0' && results[6] == '0'
   end
-end
+
+  end
+
+  #
+  # 4,5,6
+  # 7,8,9
+  #
+  # 1,4,7
+  # 2,5,8
+  # 3,6,9
+  #
+  # 1,5,9
+  # 3,5,7
--- spec/outcomes_spec.rb
+++ spec/outcomes_spec.rb
@@ -2,15 +2,40 @@ RSpec.describe 'Outcomes Checker' do
 
   let :outcomes {Outcomes.new}
 
-  win_results = ['X','X','X','0','0',nil,nil,nil,nil]
-  win_results_2 = ['0','0','0','0','0',nil,nil,nil,nil]
+  win_results = ['X','X','X',nil,nil,nil,nil,nil,nil]
+  win_results_2 = ['0','0','0',nil,nil,nil,nil,nil,nil]
+  win_results_3 = [nil,nil,nil,'X','X','X',nil,nil,nil]
+  win_results_4 = [nil,nil,nil,'0','0','0',nil,nil,nil]
+  win_results_5 = [nil,nil,nil,nil,nil,nil,'X','X','X']
+  win_results_6 = [nil,nil,nil,nil,nil,nil,'0','0','0']
+  win_results_7 = ['X',nil,nil,'X',nil,nil,'X',nil,nil]
+  win_results_8 = ['0',nil,nil,'0',nil,nil,'0',nil,nil]
+  win_results_9 = [nil,'X',nil,nil,'X',nil,nil,'X',nil]
+  win_results_10 = [nil,'0',nil,nil,'0',nil,nil,'0',nil]
+  win_results_11 = [nil,nil,'X',nil,nil,'X',nil,nil,'X']
+  win_results_12 = [nil,nil,'0',nil,nil,'0',nil,nil,'0']
+  win_results_13 = ['X',nil,nil,nil,'X',nil,nil,nil,'X']
+  win_results_14 = ['0',nil,nil,nil,'0',nil,nil,nil,'0']
+  win_results_15 = [nil,nil,'X',nil,'X',nil,'X',nil,nil]
+  win_results_16 = [nil,nil,'0',nil,'0',nil,'0',nil,nil]
 
-  it "#check method examines results & returns win or lose" do
+  it "#check method examines results & returns win" do
     expect(outcomes.check(win_results)).to eq 'win'
-  end
-
-  it "#check method examines results & returns win or lose" do
     expect(outcomes.check(win_results_2)).to eq 'win'
+    expect(outcomes.check(win_results_3)).to eq 'win'
+    expect(outcomes.check(win_results_4)).to eq 'win'
+    expect(outcomes.check(win_results_5)).to eq 'win'
+    expect(outcomes.check(win_results_6)).to eq 'win'
+    expect(outcomes.check(win_results_7)).to eq 'win'
+    expect(outcomes.check(win_results_8)).to eq 'win'
+    expect(outcomes.check(win_results_9)).to eq 'win'
+    expect(outcomes.check(win_results_10)).to eq 'win'
+    expect(outcomes.check(win_results_11)).to eq 'win'
+    expect(outcomes.check(win_results_12)).to eq 'win'
+    expect(outcomes.check(win_results_13)).to eq 'win'
+    expect(outcomes.check(win_results_14)).to eq 'win'
+    expect(outcomes.check(win_results_15)).to eq 'win'
+    expect(outcomes.check(win_results_16)).to eq 'win'
   end
 
 end
